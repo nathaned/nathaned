88d1952e4f218629a9fbad800d8c1010a35a54d4
Nathan Dabu <nathaned@users.noreply.github.com>
2017-06-02 15:55:00 -0400

    initial files upload
    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    $('.card_content').addClass('collapsed');
+    $('.expander').click(function() {
+      var selector =  $(this).parent().children('.card_content');
+        $(selector).slideToggle('fast');
+        $(selector).toggleClass('collapsed');
+        if(selector.hasClass('collapsed'))
+            $(this).html("see more");
+        else 
+            $(this).html("see less");
+    });
+    
+    
+    
+    
+    
+// fancy scroll thing i took from csstips
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+});
